17dd9e66175c9524ab57fb260eba2ed6305326f1
PumpkinPeasant <50501903+PumpkinPeasant@users.noreply.github.com>
2022-07-06 22:03:39 +0300

    Added new font

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,21 @@ void main() {
         appBar: AppBar(
           title: Text('App for gays'),
           centerTitle: true,
+          backgroundColor: Colors.amber,
         ),
         body: Text(
           'Gays are here',
           style: TextStyle(
-              fontSize: 20.0,
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.w300),
+            fontSize: 50.0,
+            letterSpacing: 3.0,
+            color: Colors.deepOrangeAccent,
+            fontFamily: "Finlandica",
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: Text('Find a Cock'),
+          backgroundColor: Colors.amber,
         ),
         // bottomNavigationBar: BottomNavigationBar(items: [],),
       ),
