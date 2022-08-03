cfe8592c828ba79214d0b2c1b228f3b795ed69c7
Anton <cqiwvi@gmail.com>
2022-08-04 00:08:41 +0300

    Чето сделал

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(FlutterApp());
+
+class FlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('App for gays'),
           centerTitle: true,
           backgroundColor: Colors.amber,
         ),
-        body: Text(
-          'Gays are here',
-          style: TextStyle(
-            fontSize: 50.0,
-            letterSpacing: 3.0,
-            color: Colors.deepOrangeAccent,
-            fontFamily: "Finlandica",
-          ),
+        body: Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("images/background.jpg"),
+                  fit: BoxFit.fill)),
         ),
+        // SvgPicture.asset("images/logo.svg"),
+        // Image.asset("images/background.jpg"),
+        // RichText(
+        //     text: TextSpan(
+        //         style: TextStyle(
+        //           fontSize: 50.0,
+        //           fontFamily: "Finlandica",
+        //         ),
+        //         children: <TextSpan>[
+        //       TextSpan(text: 'Ga', style: TextStyle(color: Colors.teal)),
+        //       TextSpan(text: 'ys', style: TextStyle(color: Colors.tealAccent)),
+        //       TextSpan(style: TextStyle(fontSize: 100.0), children: <TextSpan>[
+        //         TextSpan(text: ' a ', style: TextStyle(color: Colors.teal[50])),
+        //         TextSpan(text: 'r ', style: TextStyle(color: Colors.white)),
+        //         TextSpan(text: 'e ', style: TextStyle(color: Colors.blueAccent[100])),
+        //       ]),
+        //       TextSpan(text: 'he', style: TextStyle(color: Colors.indigo)),
+        //       TextSpan(text: 're', style: TextStyle(color: Colors.indigo[900])),
+        //     ])
+        // ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: Text('Find a Cock'),
@@ -25,6 +47,6 @@ void main() {
         ),
         // bottomNavigationBar: BottomNavigationBar(items: [],),
       ),
-    ),
-  );
+    );
+  }
 }
